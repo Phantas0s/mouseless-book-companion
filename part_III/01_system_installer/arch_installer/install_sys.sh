d094fac9ef70d61352e0c9c35348ecddd560e96f
Matthieu <matthieu.cneude@gmail.com>
2021-01-27 14:03:49 +0000

    Add first chapter of part III

---
+++ part_III/01_system_installer/arch_installer/install_sys.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+pacman -Sy
+pacman --noconfirm -S dialog
+
+timedatectl set-ntp true
+
+# Welcome message of type yesno - see `man dialog`
+dialog --defaultno --title "Are you sure?" --yesno "This is my personnal
+arch linux install. \n\n\
+It will just DESTROY EVERYTHING on the hard disk of your choice. \n\n\
+Don't say YES if you are not sure about what your are doing! \n\n\
+Are you sure?" 15 60 || exit
+
+dialog --no-cancel --inputbox "Enter a name for your computer." \
+10 60 2> comp
+
+# Verify boot (UEFI or BIOS)
+uefi=0
+ls /sys/firmware/efi/efivars 2> /dev/null && uefi=1
+
+# Choosing the hard drive
+devices_list=($(lsblk -d | awk '{print "/dev/" $1 " " $4 " on"}' \
+    | grep -E 'sd|hd|vd|nvme|mmcblk'))
+
+dialog --title "Choose your hard drive" --no-cancel --radiolist \
+"Where do you want to install your new system?\n\n\
+Select with SPACE, valid with ENTER.\n\n\
+WARNING: Everything will be DESTROYED on the hard disk!" \
+15 60 4 "${devices_list[@]}" 2> hd 
+
+hd=$(cat hd) && rm hd
+
+
